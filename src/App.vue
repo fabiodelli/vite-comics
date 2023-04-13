622dafcaaa571415a0912c8afe69f02ce4d79733
fabiodelli <ntqdel@hotmail.com>
2023-04-13 17:19:31 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,36 @@
 export default {
   data() {
     return {
-      message: "hello vite!"
+
     }
   }
 }
 </script>
 
-<template></template>
+<template>
 
-<style></style>
+  <header>
+    <div class="nav d-flex">
+      <div class="logo">
+        <img src="./assets/img/dc-logo.png" alt="">
+      </div>
+      <ul class="nav_links">
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+      </ul>
+    </div>
+  </header>
+  <main></main>
+  <footer></footer>
+  
+</template>
+
+<style lang="scss" scoped></style>
  
