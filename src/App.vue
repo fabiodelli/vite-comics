2ba17a682781ae777328733052241d23e5063c9e
fabiodelli <ntqdel@hotmail.com>
2023-04-14 15:52:54 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,9 @@ export default {
 }
 </script>
 
-<template>
 
+
+<template>
   <header>
     <div class="nav d-flex">
       <div class="logo">
@@ -29,26 +30,107 @@ export default {
     </div>
   </header>
 
+
+
   <main class="main">
 
     <div class="container_70_main"> --Content goes here-- </div>
-    
+
   </main>
+
+
 
   <footer>
 
+
+
     <div class="upper_footer">
       <div class=" container_70_upper_footer img_size ">
-        <img src="./assets/img/buy-comics-digital-comics.png" alt="">
-        <img src="./assets/img/buy-comics-merchandise.png" alt="">
-        <img src="./assets/img/buy-comics-subscriptions.png" alt="">
-        <img src="./assets/img/buy-comics-shop-locator.png" alt="">       
-        <img src="./assets/img/buy-dc-power-visa.svg" alt="">
+
+        <div>
+          <img src="./assets/img/buy-comics-digital-comics.png" alt="">
+          <span>DIGITAL COMICS</span>
+        </div>
+        <div>
+          <img src="./assets/img/buy-comics-merchandise.png" alt="">
+          <span>DC MERCHANDISE</span>
+        </div>
+        <div>
+          <img src="./assets/img/buy-comics-subscriptions.png" alt="">
+          <span>SUBSCRIPTION</span>
+        </div>
+        <div>
+          <img class="svg" src="./assets/img/buy-comics-shop-locator.png" alt="">
+          <span>COMIC SHOP LOCATOR</span>
+        </div>
+        <div>
+          <img src="./assets/img/buy-dc-power-visa.svg" alt="">
+          <span>DC POWER VISA</span>
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- central footer -->
+
+    <div class="central_footer">
+
+      <div class="central_footer_link">
+        <ul>
+          <div class="title">DC COMICS</div>
+          <div>Characters</div>
+          <div>Comics</div>
+          <div>Movies</div>
+          <div>TV</div>
+          <div>Games</div>
+          <div>Videos</div>
+          <div>News</div>
+          <div class="title">SHOP</div>
+          <div>Shop DC</div>
+          <div>News</div>
+        </ul>
+        <ul>
+          <div class="title">DC</div>
+          <div>Terms of use</div>
+          <div>Privacy polici (New)</div>
+          <div>Ad choices</div>
+          <div>Advertising</div>
+          <div>Jobs</div>
+          <div>Subscriptions</div>
+          <div>Talent worksop</div>
+          <div>CPFC certificates</div>
+          <div>Ratings</div>
+          <div>Shop help</div>
+          <div>Contact Us</div>
+        </ul>
+        <ul>
+          <div class="title">SITES</div>
+          <div>DC</div>
+          <div>Mad magazine</div>
+          <div>DC kids</div>
+          <div>DC Universe</div>
+          <div>DC Power visa</div>
+
+        </ul>
+      </div>
+    </div>
+
+    <div class="bottom_footer">
+      <div class="container_70_bottom_footer">
+        <div class="sign_up_button">SIGN UP NOW</div>
+        <div class="follow_us">
+          <strong>FOLLOW US</strong>
+          <img src="./assets/img/footer-facebook.png" alt="">
+          <img src="./assets/img/footer-periscope.png" alt="">
+          <img src="./assets/img/footer-pinterest.png" alt="">
+          <img src="./assets/img/footer-twitter.png" alt="">
+          <img src="./assets/img/footer-youtube.png" alt="">
+        </div>
       </div>
     </div>
 
   </footer>
-  
 </template>
 
 <style lang="scss" scoped></style>
